e8336e194e4e3b2b48b390582037418054176f7e
OleksandrDukach <umnick36@gmail.com>
2023-07-27 09:44:44 +0300

    q

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryPrimeEl = document.querySelector('.gallery');
+
+const newGallery = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join('');
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+galleryPrimeEl.insertAdjacentHTML('beforeend', newGallery);
+
+galleryPrimeEl.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  event.preventDefault();
+}
